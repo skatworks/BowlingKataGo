7f06019ef993df4926301aa278888bb9ec5fcd46
s.kogawa <s.kogawa@sbfw.co.jp>
2020-11-04 23:21:43 +0900

    処理を分離（リファクタリング）

--- game_test.go
+++ game_test.go
@@ -4,22 +4,25 @@ import "testing"
 
 func TestGutterGame(t *testing.T) {
      game := Game{}
-     for i := 0; i < 20; i++ {
-         game.Roll(0)
-     }
-     if game.GetScore() != 0 {
-        t.Fail()
-        t.Logf("Expected %d, got %d", 0, game.GetScore())
-     }
+     RollMany(20, 0, &game)
+     ResultCheck(0, game.GetScore(), t)
 }
 
 func Test1Pins(t *testing.T) {
      game := Game{}
-     for i := 0; i < 20; i++ {
-         game.Roll(1)
+     RollMany(20, 1, &game)
+     ResultCheck(20, game.GetScore(), t)
+}
+
+func RollMany(n int, pins int, game *Game) {
+     for i := 0; i < n; i++ {
+         game.Roll(pins)
      }
-     if game.GetScore() != 20 {
+}
+
+func ResultCheck(actual int, expected int, t *testing.T) {
+     if actual != expected {
         t.Fail()
-        t.Logf("Expected %d, got %d", 20, game.GetScore())
+        t.Logf("Expected %d, got %d", expected, actual)
      }
 }
