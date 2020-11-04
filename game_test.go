90403a17bea80b728938e8ff9660a23b5936d5b5
s.kogawa <s.kogawa@sbfw.co.jp>
2020-11-04 23:11:35 +0900

    すべて1ピンの場合を作成

--- game_test.go
+++ game_test.go
@@ -12,3 +12,14 @@ func TestGutterGame(t *testing.T) {
         t.Logf("Expected %d, got %d", 0, game.GetScore())
      }
 }
+
+func Test1Pins(t *testing.T) {
+     game := Game{}
+     for i := 0; i < 20; i++ {
+         game.Roll(1)
+     }
+     if game.GetScore() != 20 {
+        t.Fail()
+        t.Logf("Expected %d, got %d", 20, game.GetScore())
+     }
+}
